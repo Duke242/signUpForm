00cd38c18000673cb0bd643c759a3783df2b7fcf
Will Holcomb <dys@dhappy.org>
2022-11-01 02:55:47 +0000

    Update 3 files

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 window.addEventListener("load", () => {
-  console.info({ f: document.querySelector("form") });
-  document.querySelector("form").addEventListener("submit", (evt) => {
+  document.querySelector("form")?.addEventListener("submit", (evt) => {
     evt.preventDefault();
-    console.log({ evt });
   });
-});
+
+  const error = document.querySelector('#password p')
+  const passes = (
+    Array.from(document.querySelectorAll('input[type="password"]'))
+  )
+  passes.forEach((elem, idx) => {
+    elem.addEventListener('keypress', (evt) => {
+      const next = `${evt.target?.value}${evt.key}`
+      const other = passes[(idx + 1) % 2].value
+      error.style.display = (
+        next !== other ? (
+          'block'
+        ) : (
+          'none'
+        )
+      )
+    })
+  })
+})
